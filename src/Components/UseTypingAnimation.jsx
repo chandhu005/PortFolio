8b3e87485505aa0d338d2b99b9c26b19332155c5
ChandraSekhar <115386409+chandhu005@users.noreply.github.com>
2024-04-08 23:50:32 +0530

    Update UseTypingAnimation.jsx

--- src/Components/UseTypingAnimation.jsx
+++ src/Components/UseTypingAnimation.jsx
@@ -2,30 +2,20 @@ import { useState, useEffect } from "react";
 
 const useTypingAnimation = (targetWord) => {
   const [word, setWord] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
-    let currentIndex = 0;
-    let direction = 1; // 1 for forward, -1 for backward
-
     const interval = setInterval(() => {
-      if (currentIndex === targetWord.length) {
-        direction = -1;
-      } else if (currentIndex === 0) {
-        direction = 1;
-      }
-
-      setWord(targetWord.slice(0, currentIndex));
-      
-      if (direction === 1) {
-        currentIndex++;
+      if (currentIndex <= targetWord.length) {
+        setWord(targetWord.slice(0, currentIndex));
+        setCurrentIndex((prevIndex) => prevIndex + 1);
       } else {
-        currentIndex--;
+        setCurrentIndex(0);
       }
-
-    }, 300);
+    }, 150);
 
     return () => clearInterval(interval);
-  }, [targetWord]);
+  }, [targetWord, currentIndex]);
 
   return word;
 };
